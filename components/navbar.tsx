86c55aa2c3d93f6c9d37c24e137e59cff54e593e
Ingo Wolf <me@ingowolf.au>
2025-10-25 11:58:47 +1100

    change some style stuff

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -5,7 +5,7 @@ export default function Navbar() {
     <div className="w-full">
       <nav className="flex flex-row gap-2 p-2 max-w-4xl mx-auto items-center">
         <div className="flex flex-row gap-2 items-center">
-          <div className="bg-primary/25 aspect-square p-2">
+          <div className="bg-primary/25 aspect-square p-2 rounded flex justify-center items-center">
             <TerminalIcon className="text-primary" />
           </div>
           <div className="text-xl">Boot</div>
