95e76ab1e54ee270a94bdf166fc905888656824f
Ingo Wolf <me@ingowolf.au>
2025-10-25 12:46:37 +1100

    add typing animation

--- components/terminal-breadcrumbs.tsx
+++ components/terminal-breadcrumbs.tsx
@@ -1,25 +1,51 @@
+"use client";
+
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
 export default function TerminalBreadcrumbs({
   breadcrumbs = [],
 }: {
   breadcrumbs: { slug: string; text: string }[];
 }) {
-  return (
-    <div className="text-primary">
-      [you@bootos{" "}
-      <Link href="/" className="hover:underline">
-        ~
-      </Link>
-      {breadcrumbs.map((breadcrumb, index) => (
-        <span key={index}>
-          /
-          <Link href={breadcrumb.slug} className="hover:underline">
-            {breadcrumb.text}
-          </Link>
-        </span>
-      ))}
-      ]$
-    </div>
-  );
+  const content =
+    "[you@bootos ~" +
+    breadcrumbs.map((breadcrumb, index) => `/${breadcrumb.text}`) +
+    "]$";
+  const [typing, setTyping] = useState(true);
+  const [typingContent, setTypingContent] = useState("");
+
+  useEffect(() => {
+    for (let i = 0; i < content.length; i++) {
+      setTimeout(() => {
+        setTypingContent((prevContent) => prevContent + content[i]);
+      }, i * 10);
+    }
+
+    setTimeout(() => {
+      setTyping(false);
+    }, content.length * 10);
+  }, []);
+
+  if (typing) {
+    return <div className="text-primary">{typingContent}</div>;
+  } else {
+    return (
+      <div className="text-primary">
+        [you@bootos{" "}
+        <Link href="/" className="hover:underline">
+          ~
+        </Link>
+        {breadcrumbs.map((breadcrumb, index) => (
+          <span key={index}>
+            /
+            <Link href={breadcrumb.slug} className="hover:underline">
+              {breadcrumb.text}
+            </Link>
+          </span>
+        ))}
+        ]$
+      </div>
+    );
+  }
 }
