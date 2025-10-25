24aa28c14e353358d0af4bd980147f51a4cefb5f
Ingo Wolf <me@ingowolf.au>
2025-10-26 09:36:09 +1100

    use fillout form for volunteer page

--- app/volunteer/page.tsx
+++ app/volunteer/page.tsx
@@ -1,14 +1,20 @@
-import TerminalBreadcrumbs from "@/components/terminal-breadcrumbs";
+"use client";
 
-export default function Home() {
+import TerminalBreadcrumbs from "@/components/terminal-breadcrumbs";
+import { FilloutStandardEmbed } from "@fillout/react";
+
+export default function VolunteerPage() {
   return (
     <>
       <TerminalBreadcrumbs
         breadcrumbs={[
           { slug: "/", text: "home" },
-          { slug: "/volunteer", text: "volunteer" },
+          { slug: "/join", text: "join" },
         ]}
       />
+      <div className="pb-10 pt-2">
+        <FilloutStandardEmbed filloutId="" dynamicResize />
+      </div>
     </>
   );
 }
