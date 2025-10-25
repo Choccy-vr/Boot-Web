d1edf28fed2b70af832e71f82a86ecdfd469a66f
Ingo Wolf <me@ingowolf.au>
2025-10-25 11:58:23 +1100

    change icon color

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -6,7 +6,7 @@ export default function Navbar() {
       <nav className="flex flex-row gap-2 p-2 max-w-4xl mx-auto items-center">
         <div className="flex flex-row gap-2 items-center">
           <div className="bg-primary/25 aspect-square p-2">
-            <TerminalIcon />
+            <TerminalIcon className="text-primary" />
           </div>
           <div className="text-xl">Boot</div>
         </div>
