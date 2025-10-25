de2b48e0783ce6dd494d23b91d8d68226f33d312
Ingo Wolf <me@ingowolf.au>
2025-10-25 21:18:36 +1100

    change button animation a bit

--- components/button-animation.tsx
+++ components/button-animation.tsx
@@ -8,7 +8,10 @@ export default function ButtonAnimation({
   children: React.ReactNode;
 }) {
   return (
-    <motion.div whileHover={{ scale: 1.03 }} whileTap={{ scale: 0.97 }}>
+    <motion.div
+      whileHover={{ scale: 1.03, y: -1 }}
+      whileTap={{ scale: 0.97, y: 0 }}
+    >
       {children}
     </motion.div>
   );
