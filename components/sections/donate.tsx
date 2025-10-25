f8fd52094ebe61a0c6ed1288d2d74e5c4417a691
Ingo Wolf <me@ingowolf.au>
2025-10-25 22:36:12 +1100

    update donate section

--- components/sections/donate.tsx
+++ components/sections/donate.tsx
@@ -2,46 +2,48 @@ import AnimatedSection from "@/components/animations/section";
 import { Heart, Trophy, Server } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import ButtonAnimation from "../buttons/animation";
 
 export default function DonateSection() {
   return (
     <AnimatedSection>
       <h2 className="text-primary text-2xl">Support Boot</h2>
-      <div className="bg-card p-6 border rounded-xl shadow-sm">
-        <div className="flex flex-col md:flex-row items-start md:items-center gap-4 md:justify-between">
-          <div className="flex gap-4">
-            <Heart className="text-primary shrink-0" />
-            <span>
-              Your donations help us support the OS development community and
-              make Boot accessible to everyone.
-            </span>
-          </div>
+      <div className="bg-card p-6 border rounded-xl shadow-sm flex flex-row items-center">
+        <div className="flex gap-4">
+          <Heart />
+          <span>Help us create an amazing YSWS</span>
+        </div>
+        <div className="grow"></div>
+        <ButtonAnimation>
           <Button asChild size="lg" className="w-full md:w-auto">
-            <Link href="" target="_blank">
+            <Link
+              href="https://hcb.hackclub.com/donations/start/boot"
+              target="_blank"
+            >
               <Heart />
               Donate
             </Link>
           </Button>
-        </div>
+        </ButtonAnimation>
       </div>
-      <h3 className="text-xl mb-3 text-primary">Donations Help Fund</h3>
+      <h3 className="text-xl text-primary">Donations Help Fund</h3>
       <div className="grid md:grid-cols-2 gap-4">
         <div className="bg-card p-6 border rounded-xl shadow-sm">
           <div className="flex items-center gap-2 mb-3">
-            <Trophy className="text-primary" size={24} />
+            <Trophy size={24} />
             <h3 className="text-xl">Prizes & Awards</h3>
           </div>
-          <p className="text-muted-foreground">
+          <p>
             Hardware and other prizes for participants to reward creativity and
             innovation in OS development
           </p>
         </div>
         <div className="bg-card p-6 border rounded-xl shadow-sm">
           <div className="flex items-center gap-2 mb-3">
-            <Server className="text-primary" size={24} />
+            <Server size={24} />
             <h3 className="text-xl">Infrastructure</h3>
           </div>
-          <p className="text-muted-foreground">
+          <p>
             Cloud VMs, servers, and resources to keep Boot running smoothly for
             all participants
           </p>
