4576214c21585ccfc82fb0e3a62d5e800ecaceb1
Ingo Wolf <me@ingowolf.au>
2025-10-25 12:04:44 +1100

    change text color and sizing

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -4,11 +4,11 @@ export default function Navbar() {
   return (
     <div className="w-full">
       <nav className="flex flex-row gap-2 p-2 max-w-4xl mx-auto items-center">
-        <div className="flex flex-row gap-2 items-center">
+        <div className="flex flex-row gap-4 items-center">
           <div className="bg-accent border border-primary shadow-2xl aspect-square p-2 rounded flex justify-center items-center">
             <TerminalIcon className="text-primary" />
           </div>
-          <div className="text-xl">Boot</div>
+          <div className="text-2xl text-primary">Boot</div>
         </div>
       </nav>
     </div>
