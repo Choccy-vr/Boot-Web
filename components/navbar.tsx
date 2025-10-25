24b7ba491bc31b1812510a8e684022aee890dce7
Ingo Wolf <me@ingowolf.au>
2025-10-25 12:26:09 +1100

    make logo go home

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -7,12 +7,12 @@ export default function Navbar() {
   return (
     <div className="w-full">
       <nav className="flex flex-row gap-2 p-2 max-w-4xl mx-auto items-center">
-        <div className="flex flex-row gap-4 items-center">
+        <Link href="/" className="flex flex-row gap-4 items-center">
           <div className="bg-accent border border-primary shadow-2xl aspect-square p-2 rounded flex justify-center items-center">
             <Terminal className="size-4 text-primary" />
           </div>
           <div className="text-2xl text-primary">Boot</div>
-        </div>
+        </Link>
         <div className="grow"></div>
         <Button variant="secondary" asChild>
           <Link href="/volunteer">Volunteer</Link>
