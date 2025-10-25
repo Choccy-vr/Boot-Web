bed0e120d2822439b68128a8f8e095cebebca73c
Ingo Wolf <me@ingowolf.au>
2025-10-26 09:05:38 +1100

    add footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import Navbar from "@/components/navbar";
 import JoinButton from "@/components/buttons/join";
+import Footer from "@/components/footer";
 
 const geistMono = Geist_Mono({
   subsets: ["latin"],
@@ -35,6 +36,7 @@ export default function RootLayout({
         <ThemeProvider attribute="class" forcedTheme="dark">
           <Navbar />
           <main className="max-w-4xl w-screen mx-auto px-2">{children}</main>
+          <Footer />
           <JoinButton className="fixed bottom-2 right-2" size="lg" />
         </ThemeProvider>
       </body>
---
+++ components/footer.tsx
@@ -0,0 +1,38 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Footer() {
+  return (
+    <footer className="py-10">
+      <div className="max-w-4xl w-full mx-auto px-2 flex flex-col gap-2">
+        <div className="flex flex-row gap-2 items-center">
+          <Image alt="Boot Logo" src="/icon.png" width={36} height={36} />
+          <div className="text-primary text-2xl">Boot</div>
+        </div>
+        <p>
+          A YSWS (You Ship We Ship) where teens from around the world build
+          operating systems.
+        </p>
+        <p>
+          Boot is fiscally sponsored by The Hack Foundation (d.b.a. Hack Club),
+          a 501(c)(3) nonprofit (EIN: 81-2908499).
+        </p>
+        <div className="flex flex-row gap-4">
+          <Link href="/volunteer" className="underline">
+            Volunteer
+          </Link>
+          <Link href="mailto:gino@boot-os.com" className="underline">
+            Sponsor Us
+          </Link>
+          <Link
+            href="https://hcb.hackclub.com/donations/start/boot"
+            target="_blank"
+            className="underline"
+          >
+            Donate
+          </Link>
+        </div>
+      </div>
+    </footer>
+  );
+}
