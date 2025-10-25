7d2b2b9183ebe1148adb782adf3f46b946d69975
Ingo Wolf <me@ingowolf.au>
2025-10-25 21:16:28 +1100

    allow passing props down

--- components/animated-section.tsx
+++ components/animated-section.tsx
@@ -7,11 +7,12 @@ export default function AnimatedSection({
   children,
   className,
   delay = 0,
+  ...props
 }: {
   children: React.ReactNode;
   className?: string;
   delay?: number;
-}) {
+} & React.ComponentProps<typeof motion.div>) {
   return (
     <motion.div
       initial={{ opacity: 0, y: 20 }}
@@ -19,6 +20,7 @@ export default function AnimatedSection({
       viewport={{ once: true }}
       transition={{ ease: [0.165, 0.84, 0.44, 1.0], delay: delay }}
       className={clsx("pb-4 flex flex-col gap-2", className)}
+      {...props}
     >
       {children}
     </motion.div>
