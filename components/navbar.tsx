cf0e4c018e830b630489ce330a6fca8c71f94aad
Ingo Wolf <me@ingowolf.au>
2025-10-26 21:38:57 +1100

    add guides link

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -16,11 +16,11 @@ export default function Navbar() {
           </Link>
         </ButtonAnimation>
         <div className="grow"></div>
-        {/*<ButtonAnimation>
+        <ButtonAnimation>
           <Button variant="ghost" asChild>
-            <Link href="/guide">Guides</Link>
+            <Link href="/guides/home">Guides</Link>
           </Button>
-        </ButtonAnimation>*/}
+        </ButtonAnimation>
         <ButtonAnimation>
           <Button variant="secondary" asChild>
             <Link href="/volunteer">Volunteer</Link>
