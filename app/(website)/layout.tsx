ffbe78afca1b473d4756c72de0708193d6934f49
Ingo Wolf <me@ingowolf.au>
2025-10-26 13:36:45 +1100

    remove metadata from layout

--- app/(website)/layout.tsx
+++ app/(website)/layout.tsx
@@ -1,23 +1,7 @@
-import type { Metadata } from "next";
 import Navbar from "@/components/navbar";
 import JoinButton from "@/components/buttons/join";
 import Footer from "@/components/footer";
 import AnimatedSection from "@/components/animations/section";
-
-export const metadata: Metadata = {
-  title: "Boot - Build you OS, get hardware to run it",
-  description: "Build your OS, Get hardware to run it. A Hack Club YSWS.",
-  keywords: [
-    "ysws",
-    "hackathon",
-    "operating system",
-    "os",
-    "linux",
-    "hack club",
-    "programming",
-    "teens",
-  ],
-};
 
 export default function RootLayout({
   children,
