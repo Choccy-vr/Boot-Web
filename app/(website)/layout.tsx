9bd393acb616323ae6d4bea1ffd8edda4a272afb
Ingo Wolf <me@ingowolf.au>
2025-10-26 18:51:34 +1100

    remove animation from footer

--- app/(website)/layout.tsx
+++ app/(website)/layout.tsx
@@ -1,7 +1,6 @@
 import Navbar from "@/components/navbar";
 import JoinButton from "@/components/buttons/join";
 import Footer from "@/components/footer";
-import AnimatedSection from "@/components/animations/section";
 
 export default function RootLayout({
   children,
@@ -14,9 +13,7 @@ export default function RootLayout({
         <Navbar />
         <main className="max-w-4xl w-screen mx-auto p-4">{children}</main>
         <div className="grow"></div>
-        <AnimatedSection>
-          <Footer />
-        </AnimatedSection>
+        <Footer />
       </div>
       <JoinButton className="fixed bottom-2 right-2" size="lg" />
     </>
