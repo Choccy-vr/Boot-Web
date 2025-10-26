abc083894337bdc69ab2df46ec0a10167472773a
Ingo Wolf <me@ingowolf.au>
2025-10-26 13:22:17 +1100

    add guide pages

---
+++ app/(guide)/layout.tsx
@@ -0,0 +1,13 @@
+import { RootProvider } from "fumadocs-ui/provider/next";
+import type { ReactNode } from "react";
+
+export default function Layout({ children }: { children: ReactNode }) {
+  return (
+    <div
+      // required styles
+      className="flex flex-col min-h-screen"
+    >
+      <RootProvider>{children}</RootProvider>
+    </div>
+  );
+}
