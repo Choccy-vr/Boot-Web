c52560490a38f72fda895d9c8ae629bdd275c85a
Ingo Wolf <me@ingowolf.au>
2025-12-17 11:33:44 +1100

    add hack club flag

--- app/(website)/page.tsx
+++ app/(website)/page.tsx
@@ -3,6 +3,7 @@ import HeroSection from "@/components/sections/hero";
 import AboutSection from "@/components/sections/about";
 import WhatCountsSection from "@/components/sections/whatcounts";
 import FAQsSection from "@/components/sections/faqs";
+import Image from "next/image";
 
 export default function Home() {
   return (
@@ -12,6 +13,13 @@ export default function Home() {
       <AboutSection />
       <WhatCountsSection />
       <FAQsSection />
+      <Image
+        src="flag-orpheus-left.svg"
+        alt="Hack Club"
+        width={280}
+        height={158}
+        className="fixed bottom-4 left-0 z-50 w-40 -translate-x-2 -rotate-3 hover:translate-x-0 hover:rotate-0 duration-200"
+      />
     </div>
   );
 }
