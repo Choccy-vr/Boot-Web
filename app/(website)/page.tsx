916800d2e3a5c8d4f4481fddcb659976f4912d73
Ingo Wolf <me@ingowolf.au>
2025-12-17 11:37:25 +1100

    add link

--- app/(website)/page.tsx
+++ app/(website)/page.tsx
@@ -4,6 +4,7 @@ import AboutSection from "@/components/sections/about";
 import WhatCountsSection from "@/components/sections/whatcounts";
 import FAQsSection from "@/components/sections/faqs";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -13,13 +14,15 @@ export default function Home() {
       <AboutSection />
       <WhatCountsSection />
       <FAQsSection />
-      <Image
-        src="flag-orpheus-left.svg"
-        alt="Hack Club"
-        width={280}
-        height={158}
-        className="fixed bottom-4 left-0 z-50 w-40 -translate-x-2 -rotate-3 hover:translate-x-0 hover:rotate-0 duration-200"
-      />
+      <Link href="https://hackclub.com" target="_blank">
+        <Image
+          src="flag-orpheus-left.svg"
+          alt="Hack Club"
+          width={280}
+          height={158}
+          className="fixed bottom-4 left-0 z-50 w-40 -translate-x-2 -rotate-3 hover:translate-x-0 hover:rotate-0 duration-200"
+        />
+      </Link>
     </div>
   );
 }
