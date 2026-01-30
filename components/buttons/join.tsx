4483d6b93b7014a0afce6446aae424d6cfae1f49
Ginobeano <94711504+Choccy-vr@users.noreply.github.com>
2026-01-30 14:11:07 -0500

    Change JoinButton text and link destination
    Change to go to dashboard

--- components/buttons/join.tsx
+++ components/buttons/join.tsx
@@ -3,12 +3,12 @@ import { Button } from "../ui/button";
 import Link from "next/link";
 
 export default function JoinButton({
-  text = "Join",
+  text = "Go To Dashboard",
   ...props
 }: React.ComponentProps<typeof Button> & { text?: string }) {
   return (
     <Button {...props} asChild>
-      <Link href="/join">
+      <Link href="/dashboard">
         <Rocket />
         {text}
       </Link>
